b36cf33344735a2eab325b1d8bc96df8571c376b
Adrian Almenar <aalmenar@gmail.com>
2014-04-03 23:23:55 +0200

    Bump to version 0.0.7

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'aalmenar@gmail.com'
 license          'Apache 2.0'
 description      'Adds new locales and generates them'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.4'
+version          '0.0.7'
 
 supports 'ubuntu'
 supports 'debian'
