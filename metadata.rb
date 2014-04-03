5fb87f97c474c6377ca167d714e6070a752b3181
Adrian Almenar <aalmenar@gmail.com>
2014-04-03 21:13:02 +0200

    Reindent file correctly

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name			 "locale-gen"
-maintainer       "Dan Hart"
-maintainer_email "dan@danhart.co.uk"
-license          "Apache 2.0"
-description      "Adds new locales and generates them"
+name             'locale-gen'
+maintainer       'Dan Hart'
+maintainer_email 'dan@danhart.co.uk'
+license          'Apache 2.0'
+description      'Adds new locales and generates them'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          '0.0.3'
 
-supports "ubuntu"
-supports "debian"
+supports 'ubuntu'
+supports 'debian'
